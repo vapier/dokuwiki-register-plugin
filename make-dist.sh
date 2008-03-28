3bd158a9ffa0bc42d4360e27a0935a66bdc9eda1
Mike Frysinger <vapier@gentoo.org>
2008-03-28 11:46:09 -0400

    handle dist

---
+++ make-dist.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ver=$(git-rev-parse HEAD | cut -c1-8)
+mkdir -p dist/register
+cp -a register.php{,s} syntax.php{,s} dist/register/
+cd dist
+tar zcf register.tar.gz register
+mv register.tar.gz ..
+cd ..
+rm -r dist
